39b96e1000ad50eacc16ab13a9e404f3761a2495
DaveO <dufferdo2@gmail.com>
2019-06-29 17:29:01 -0700

    auto link of parcel/brunch stripcode plugins

---
+++ public/brunch-config.js
@@ -0,0 +1,114 @@
+const path = require('path')
+const isProduction = process.env.NODE_ENV === 'production'
+const deployDir = isProduction ? 'dist/brunch' : 'dist_test/brunch'
+const fontLocation = isProduction ? '../fonts' : process.env.USE_WATCH === 'true' ? 'fonts' : '../fonts'
+const singleRun = process.env.USE_HMR !== 'true' && !process.env.USE_TDD
+const htmlFile = isProduction ? 'brunch/appl/testapp.html' : 'brunch/appl/testapp_dev.html'
+
+function resolve(dir) {
+  return path.join(__dirname, 'brunch', dir)
+}
+
+exports.paths = {
+  public: deployDir,
+  watched: ['brunch/appl', 'brunch/jasmine']
+};
+
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^(?!brunch\/appl)/,
+      'acceptance.js': [/^brunch\/appl/, /^brunch\/jasmine/]
+    }
+  },
+  templates: {
+    joinTo: 'acceptance.js'
+  },
+  stylesheets: {
+    joinTo: 'acceptance.css',
+    order: {
+      after: [
+        "brunch/appl/css/site.css"
+      ]
+    }
+  }
+};
+
+pluginsObject = {
+  stripcode: {
+    start: 'develblock:start',
+    end: 'develblock:end'
+  },
+  // See README.md for implementation
+  // eslint: {
+  //   pattern: /^brunch\/appl\/.*\.js?$/,
+  //   warnOnly: true
+  // },
+  brunchTypescript: {
+    "baseUrl": "./",
+    "outDir": "",
+    "typeRoots": [
+      "node_modules/@types"
+    ],
+    ignoreErrors: false,
+    allowJs: true
+  },
+  copycat: {
+    'views': ['brunch/appl/views'],
+    'templates': ['brunch/appl/templates'],
+    './': ['README.md', htmlFile, 'brunch/appl/app_bootstrap.html'],
+    'images': ['brunch/images'],
+    'css': ["brunch/appl/css/table.css", "brunch/appl/css/hello.world.css"],
+    verbose: false,
+    onlyChanged: true
+  }
+};
+
+pluginsObject.copycat[fontLocation] = ['node_modules/font-awesome/fonts']
+exports.plugins = pluginsObject
+
+exports.npm = {
+  enabled: true,
+  globals: {
+    jQuery: 'jquery',
+    $: 'jquery',
+    bootstrap: 'bootstrap',
+    Popper: 'popper.js'
+  },
+  styles: {
+    bootstrap: ['dist/css/bootstrap.css'],
+    'font-awesome': ['css/font-awesome.css'],
+    tablesorter: [
+      'dist/css/jquery.tablesorter.pager.min.css',
+      'dist/css/theme.blue.min.css'
+    ]
+  },
+  aliases: {
+    handlebars: "handlebars/dist/handlebars.min.js",
+    pager: "tablesorter/dist/js/extras/jquery.tablesorter.pager.min.js"
+  }
+};
+
+exports.server = {
+  port: 3080,
+  base: '/',
+  stripSlashes: true
+}
+
+pluginsObject.karma = require('./brunch/build/karma.conf')
+pluginsObject.karma.singleRun = singleRun
+
+exports.overrides = {
+  production: {
+    paths: {
+      watched: ['brunch/appl']
+    },
+    conventions: {
+      ignored: ['brunch/jasmine']
+    },
+    plugins: {
+      off: ['karma']
+    }
+  }
+}
+
